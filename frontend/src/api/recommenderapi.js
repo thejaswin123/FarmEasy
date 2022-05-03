f78071ed3edb91a5b2f58fe30d6ac45f55add23b
Thejaswin <thejas2002@yahoo.in>
2022-05-03 17:50:36 +0530

    final

--- frontend/src/api/recommenderapi.js
+++ frontend/src/api/recommenderapi.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL:"https://b12-farmeasy.herokuapp.com/",
+    baseURL:"https://b12-farmeasy.herokuapp.com",
     headers: { "Content-Type": "multipart/form-data" },
 })
